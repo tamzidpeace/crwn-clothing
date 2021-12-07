af69b531fdf867ffcef9d5e292f37199930acab9
tamzidpeace <tamjedpeace@gmail.com>
2021-12-07 23:10:27 +0600

    added image

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -3,6 +3,7 @@ import MenuItem from "../menu-item/menu_item.component";
 import './directory.styles.scss'
 
 class Directory extends React.Component {
+
     constructor() {
         super();
         this.state = {
@@ -47,7 +48,14 @@ class Directory extends React.Component {
         return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({ title, imageUrl, id }) => <MenuItem key={id} title={title} />)
+                    this.state.sections.map(({ title, imageUrl, id, size }) =>
+                        <MenuItem
+                            key={id}
+                            title={title}
+                            imageUrl={imageUrl}
+                            size={size}
+                        />
+                    )
                 }
             </div>
         )
--- src/components/menu-item/menu_item.component.jsx
+++ src/components/menu-item/menu_item.component.jsx
@@ -1,7 +1,9 @@
 import './menu_item.styles.scss'
 
-const MenuItem = ({ title }) => (
-    <div className='menu-item'>
+const MenuItem = ({ title, imageUrl, size }) => (
+    <div
+        style={{ backgroundImage: `url(${imageUrl})` }}
+        className={`${size} menu-item`}>
         <div className="content">
             <h1 className="title">{title}</h1>
             <span className="subtitle">Shop Now</span>
