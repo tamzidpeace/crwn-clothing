3eeb20d8bfbc20ed964acfb15a8a38d795ec78b1
tamzidpeace <tamjedpeace@gmail.com>
2021-12-07 22:13:03 +0600

    component wise structrued

---
+++ src/components/menu-item/menu_item.component.jsx
@@ -0,0 +1,13 @@
+import './menu_item.styles.scss'
+
+const MenuItem = ({ title }) => (
+    <div className='menu-item'>
+        <div className="content">
+            <h1 className="title">{title}</h1>
+            <span className="subtitle">Shop Now</span>
+        </div>
+    </div>
+)
+
+
+export default MenuItem
