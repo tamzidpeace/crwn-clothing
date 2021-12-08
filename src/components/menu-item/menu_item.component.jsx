3623112161b08a62a5157154eeb3b35ec908f837
tamzidpeace <tamjedpeace@gmail.com>
2021-12-08 09:08:17 +0600

    lesson 3 complete

--- src/components/menu-item/menu_item.component.jsx
+++ src/components/menu-item/menu_item.component.jsx
@@ -1,15 +1,20 @@
-import './menu_item.styles.scss'
+
+
+import './menu_item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
     <div
-        style={{ backgroundImage: `url(${imageUrl})` }}
-        className={`${size} menu-item`}>
-        <div className="content">
-            <h1 className="title">{title}</h1>
-            <span className="subtitle">Shop Now</span>
-        </div>
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
     </div>
-)
+  </div>
+);
 
-
-export default MenuItem
+export default MenuItem;
