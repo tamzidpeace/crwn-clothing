dd18425f6a7f754b8b1474f838a9819a98cbf3fe
tamzidpeace <tamjedpeace@gmail.com>
2021-12-10 20:44:38 +0600

    shop data rendered

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import './App.css';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
+
 
 function App() {
   return (
@@ -14,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route path='/hats' component={HatsPage} />
+          <Route path='/shop' component={ShopPage} />
         </Switch>
       </BrowserRouter>
     </div>
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../../components/preview-collection/collection-preview.component";
 
 class ShopPage extends React.Component {
     constructor(pros) {
@@ -9,6 +10,20 @@ class ShopPage extends React.Component {
             collections : SHOP_DATA
         }
     } 
+
+    render() {
+        const { collections } = this.state;
+        return (
+            <div className="shop-page">
+                {
+                    collections.map(({ id, ...othersCollectionProps }) =>
+                    (
+                        <CollectionPreview key={id} {...othersCollectionProps}/>
+                    ))
+                }
+            </div>
+        )
+    }
 }
 
 
