86a9ef474b63c447a24cfe55bfa5a022df8b59b1
tamzidpeace <tamjedpeace@gmail.com>
2021-12-27 09:59:27 +0600

    custom button redesign

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route exact path='/sign-in' render={() => this.props.currentUser? (<Redirect to='/' />) : (SignInAndSignUpPage)}/>
+          <Route exact path='/sign-in' render={() => this.props.currentUser? (<Redirect to='/' />) : (<SignInAndSignUpPage/>)}/>
         </Switch>
 
       </div>
