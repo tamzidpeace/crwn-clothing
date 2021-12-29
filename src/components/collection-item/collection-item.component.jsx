57181191d9395005d7982ca023a1a0d9923dac6b
tamzidpeace <tamjedpeace@gmail.com>
2021-12-29 08:34:21 +0600

    cart redux is running

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import './collection-item.styles.scss';
 import CustomButton from "../custom-button/custom-button.component";
+import { connect } from "react-redux";
+import { addItem } from "../../redux/cart/cart.actions";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+const CollectionItem = ({ id, name, price, imageUrl, addItem }) => (
     <div className="collection-item">
 
         <div className="image" style={{ backgroundImage: `url(${imageUrl})` }}></div>
@@ -17,4 +19,8 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
     </div>
 )
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps) (CollectionItem);
